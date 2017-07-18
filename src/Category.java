b2be8971c81c7419991871c4ebd0bb5a91aefc4c
FransLukas <franslukas@gmail.com>
2017-07-18 10:54:38 +0200

    You can now choose category to search in.

---
+++ src/Category.java
@@ -0,0 +1,22 @@
+/**
+ * Created by frasse on 2017-07-18.
+ */
+public class Category {
+
+    private String categoryNumber;
+    private String category;
+
+    public Category(String categoryNumber, String category){
+        this. categoryNumber = categoryNumber;
+        this.category = category;
+    }
+
+    public String getCategoryNumber() {
+        return categoryNumber;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -6,7 +7,41 @@ public class Main {
 	// write your code here
         Spider crawler = new Spider();
         try {
-            crawler.crawl("http://www.ebay.com/sch/i.html?_nkw=&_in_kw=1&_ex_kw=&_sacat=220&_udlo=&_udhi=&LH_BIN=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=&_sargn=-1%26saslc%3D1&_salic=1&_sop=15&_dmd=1&_ipg=50");
+            Category categories[] = {new Category("11450", "Clothing & Accessories"),
+                                        new Category("220", "Toys"),
+                                        new Category("940", "Lines & Textiles"),
+                                        new Category("293", "Electronics"),
+                                        new Category("267", "Books"),
+                                        new Category("15032", "Cellphones & Accessories"),
+                                        new Category("1", "Collectibles"),
+                                        new Category("237", "Dolls & Bears"),
+                                        new Category("260", "Stamps"),
+                                        new Category("26395", "Health and Beauty"),
+                                        new Category("11700", "Home & Garden"),
+                                        new Category("281", "Jewelry & Watches"),
+                                        new Category("619", "Musical Instruments and Gear"),
+                                        new Category("870", "Pottery & Glass"),
+                                        new Category("888", "Sporting Goods"),
+                                        new Category("3252", "Travel"),
+                                        new Category("172008", "Gift Cards & Coupons"),
+                                        new Category("99", "Everything else")};
+            System.out.println("Choose category number to search in: \n");
+
+            int index = 0;
+            for (Category category : categories) {
+                System.out.println((index + 1) + ". " + category.getCategory());
+                index++;
+            }
+            System.out.println();
+            Scanner reader = new Scanner(System.in);
+            index = reader.nextInt() - 1;
+            System.out.println(categories[index].getCategory() + ", " + categories[index].getCategoryNumber());
+            reader.next();
+
+            crawler.crawl("http://www.ebay.com/sch/i.html?_nkw=&_in_kw=1&_ex_kw=&_sacat=" +
+                    categories[index].getCategoryNumber() +
+                    "&_udlo=&_udhi=&LH_BIN=1&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15" +
+                    "&_stpos=&_sargn=-1%26saslc%3D1&_salic=1&_sop=15&_dmd=1&_ipg=50");
         } catch (IOException e) {
             e.printStackTrace();
         }
